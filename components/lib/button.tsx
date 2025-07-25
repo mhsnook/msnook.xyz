92f439b8ccf2ef91e87db30717012524af3f6604
Em Snook <me@msnook.xyz>
2025-07-25 15:49:38 +0530

    Change to TS

--- components/lib/button.tsx
+++ components/lib/button.tsx
@@ -1,13 +1,19 @@
 import { cn } from '@/lib/utils'
 import { buttonStyles } from '@/components/lib'
 
+type ButtonProps = {
+	className?: string
+	variant?: string
+	size?: string
+} & React.ButtonHTMLAttributes<HTMLButtonElement>
+
 export default function Button({
 	className = '',
 	variant,
 	size,
 	children,
 	...props
-}) {
+}: ButtonProps) {
 	return (
 		<button
 			className={cn(buttonStyles({ variant, size }), className)}
