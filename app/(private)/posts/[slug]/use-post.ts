53cfa8e5bf0e1633997e812efcb1551dceb70e12
Em Snook <me@msnook.xyz>
2025-07-25 22:19:30 +0530

    Redo the edit page to use suspense query instead of SSR

--- app/(private)/posts/[slug]/use-post.ts
+++ app/(private)/posts/[slug]/use-post.ts
@@ -3,7 +3,7 @@ import { queryOptions } from '@tanstack/react-query'
 
 export const postQueryOptions = (slug: string) =>
 	queryOptions({
-		queryKey: ['posts', slug],
+		queryKey: ['post', slug],
 		queryFn: async ({ queryKey }) => await fetchOnePost(queryKey[1]),
 		refetchOnWindowFocus: false,
 		refetchOnReconnect: false,
